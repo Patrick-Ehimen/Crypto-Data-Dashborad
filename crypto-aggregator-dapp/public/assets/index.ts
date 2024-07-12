73b86ce660ddaf360058894ad027323deca029bd
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-07-12 08:45:02 +0100

    create UI components

---
+++ crypto-aggregator-dapp/public/assets/index.ts
@@ -0,0 +1,69 @@
+import Arbitrum from "./icons/arbitrum.svg";
+import Avalanche from "./icons/Avalance.svg";
+import Base from "./icons/Base.svg";
+import BSC from "./icons/BinanceSmartChain.svg";
+import ETH from "./icons/Ethereum.svg";
+import Optimism from "./icons/Optimism.svg";
+import Polygon from "./icons/Polygon.svg";
+import MarginTrade from "./icons/MarginTrade.svg";
+import MobileApp from "./icons/MobileApp.svg";
+import News from "./icons/News.svg";
+import TradeAPI from "./icons/TradeAPI.svg";
+import Wallet from "./icons/Wallet.svg";
+import Widgets from "./icons/Widget.svg";
+import MetamaskWallet from "./wallets/MetamaskWallet.svg";
+import PhantomWallet from "./wallets/PhantomWallet.svg";
+import CoinbaseWallet from "./wallets/CoinbaseWallet.svg";
+import WalletConnectWallet from "./wallets/WalletConnectWallet.svg";
+import Oxose from "./oxose.jpeg";
+import BitcoinIcon from "./icons/BitcoinIcon.svg";
+import UsdtIcon from "./icons/UsdtIcon.svg";
+import SolanaIcon from "./icons/SolanaIcon.svg";
+import UsdcIcon from "./icons/UsdcIcon.svg";
+import TronIcon from "./icons/TronIcon.svg";
+import Squre1 from "./icons/square1.svg";
+import Squre2 from "./icons/square2.svg";
+import Squre3 from "./icons/square3.svg";
+import Squre4 from "./icons/square4.svg";
+import NewsImage1 from "./wallets/NewsImage1.png";
+import NewsImage2 from "./wallets/NewsImage2.png";
+import NewsImage3 from "./wallets/NewsImage3.png";
+import NewsImage4 from "./wallets/NewsImage4.png";
+import NewsImage5 from "./wallets/NewsImage5.png";
+import NewsImage6 from "./wallets/NewsImage6.png";
+
+export {
+  Arbitrum,
+  Avalanche,
+  Base,
+  BSC,
+  ETH,
+  Optimism,
+  Polygon,
+  MarginTrade,
+  MobileApp,
+  News,
+  TradeAPI,
+  Wallet,
+  Widgets,
+  MetamaskWallet,
+  PhantomWallet,
+  CoinbaseWallet,
+  WalletConnectWallet,
+  Oxose,
+  BitcoinIcon,
+  UsdtIcon,
+  SolanaIcon,
+  UsdcIcon,
+  TronIcon,
+  Squre1,
+  Squre2,
+  Squre3,
+  Squre4,
+  NewsImage1,
+  NewsImage2,
+  NewsImage3,
+  NewsImage4,
+  NewsImage5,
+  NewsImage6,
+};
---
+++ crypto-aggregator-dapp/src/app/(demo)/markets/page.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+// import CoinsContent from "@/components/page-contents/coins-content";
+import PlaceholderContent from "@/components/page-contents/placeholder-content";
+import { ContentLayout } from "@/components/sidebar-panel/content-layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function MarketPage() {
+  return (
+    <ContentLayout title="All Rankings">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Coins</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* <CoinsContent /> */}
+      <PlaceholderContent />
+    </ContentLayout>
+  );
+}
